098614a7df42e26b23f2244e160c3249916d152f
Vinicius Zaniboni <viny@qpilot.cloud>
2024-05-02 11:49:05 -0300

    fix build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,32 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import 'qpilot-delivery-date';
-import 'nextime-delivery-date';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { SiteComponent } from 'src/app/site/site.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Site2Component } from 'src/app/site2/site2.component';
-import { DateSuffix } from 'src/app/date-suffix.pipe';
-import { NgxJsonViewerModule } from 'ngx-json-viewer';
-import { NewSiteComponent } from 'src/app/new-site/new-site.component';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatNativeDateModule } from '@angular/material/core';
 import {
-  MomentDateAdapter,
   MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  MomentDateAdapter,
 } from '@angular/material-moment-adapter';
 import {
   DateAdapter,
   MAT_DATE_FORMATS,
-  MAT_DATE_LOCALE,
+  MAT_DATE_LOCALE, MatNativeDateModule
 } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'nextime-delivery-date';
+import { NgxJsonViewerModule } from 'ngx-json-viewer';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { DateSuffix } from 'src/app/date-suffix.pipe';
+import { NewSiteComponent } from 'src/app/new-site/new-site.component';
+import { SiteComponent } from 'src/app/site/site.component';
+import { Site2Component } from 'src/app/site2/site2.component';
+import { AppComponent } from './app.component';
 
 // Depending on whether rollup is used, moment needs to be imported differently.
 // Since Moment.js doesn't have a default export, we normally need to import using the `* as`
 // syntax. However, rollup creates a synthetic default module and we thus need to import it using
 // the `default as` syntax.
-import * as moment from 'moment';
 
 // See the Moment.js docs for the meaning of these formats:
 // https://momentjs.com/docs/#/displaying/format/
